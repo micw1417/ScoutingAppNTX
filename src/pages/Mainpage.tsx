18f4da120e7550cc87d4d46a4e885e721723e0c4
micw1417 <wu355049@gmail.com>
2024-09-07 13:53:53 -0500

    proof of concept

--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -1,20 +1,29 @@
-
 // export interface TimeDataProps {
 //   timeElapsed: number;
 //   timeSinceLastScore: number;
 // }
 
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
 
-import React from 'react'
+const Mainpage: React.FC = () => {
+  const [thing, setThing] = useState('')
+  const [data, setData] = useState<any>()
 
-const Mainpage:  React.FC = () => {
+  function submit() {
+    setData({thing});
+  }
+
   return (
     <>
-      <h1>
-        Scouting App!!
-      </h1>
+      <h1>Scouting App!!</h1>
+      <input placeholder="do th thing" value={thing} onChange={e => setThing(e.target.value)}/>
+
+      <button onClick={submit}>Submit</button>
+
+      {data && <QRCode value={JSON.stringify(data)}/>}
     </>
-  )
-}   
-  
-export default Mainpage
+  );
+};
+
+export default Mainpage;
